b54bb066e0717c5ee0fc715187e365ac1aa30b58
Ben <bweinberg@brandeis.edu>
2019-09-07 19:58:41 -0600

    Updated collisions
    To work better with platforms in the future, and so we don't eventually run into a problem colliding with enemies.

--- Code/JS/TestLevel.js
+++ Code/JS/TestLevel.js
@@ -14,15 +14,21 @@ class TestLevel extends Phaser.Scene{
         this.floor.body.setAllowGravity(false);
         this.floor.setImmovable(true);
         character = this.physics.add.sprite(300,0,"pika"); 
+        character.setCollideWorldBounds(true);
+        character.body.setBounce(1,1);
         this.input.keyboard.on("keyup_X", function(event){
             character.setVelocity(character.body.velocity.x, character.body.velocity.y - 100);
         }, this);
 
-        this.physics.add.collider(character, this.floor);
+        this.physics.add.collider(character, this.floor, this.hitPlatform, null, this);
     }
     update(delta){
-        //if(character.body.onFloor()) {
-        //   character.setVelocity(character.body.velocity.x, -10);
-        //}
+        //Nothing yet
+    }
+
+    hitPlatform(character, floor) {
+        if(character.body.touching.down && !character.body.touching.left && !character.body.touching.right) { //Collisions with just the bottom of the player don't cause y bounce
+            character.body.setVelocity(character.body.velocity.x,0);
+        }
     }
 }
